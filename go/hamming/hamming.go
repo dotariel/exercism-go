af2f74cb0edbd240c1df04f9454f4d5a0e0858fd
Ariel Alonso <xsaint32@gmail.com>
2017-07-07 11:29:03 -0400

    Remove comments and fmt files

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -8,13 +8,10 @@ const testVersion = 6
 
 func Distance(a, b string) (int, error) {
 
-	// Return right away if the strings are not comparable
 	if len(a) != len(b) {
 		return -1, errors.New("Strings must be of equal length")
 	}
 
-	// Loop through one string and compare the value at each
-	// position with the other string, counting the differences.
 	hamming := 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,5 +3,5 @@ package leap
 const testVersion = 3
 
 func IsLeapYear(year int) bool {
-  return (year%400 == 0) || (year%4 == 0 && year%100 != 0)
+	return (year%400 == 0) || (year%4 == 0 && year%100 != 0)
 }
