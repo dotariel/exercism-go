c4ef6a22462fd343c1e7ea50fd81de66ceb7acee
Ariel Alonso <xsaint32@gmail.com>
2017-07-07 21:48:13 -0400

    Defer resource.Close() in a func()

--- go/error-handling/common.go
+++ go/error-handling/common.go
@@ -1,6 +1,8 @@
 package erratum
 
-import "io"
+import (
+	"io"
+)
 
 const testVersion = 2
 
@@ -40,3 +42,28 @@ type Resource interface {
 // It may return a wrapped error of type TransientError. In this case the resource
 // is temporarily unavailable and the caller should retry soon.
 type ResourceOpener func() (Resource, error)
+
+func Use(o ResourceOpener, input string) (err error) {
+
+	res, err := o()
+
+	if err != nil {
+		if _, ok := err.(TransientError); ok {
+			return Use(o, input)
+		}
+		return err
+	}
+
+	defer func() {
+		defer res.Close()
+		if p := recover(); p != nil {
+			if ferr, ok := p.(FrobError); ok {
+				res.Defrob(ferr.defrobTag)
+			}
+			err = p.(error)
+		}
+	}()
+
+	res.Frob(input)
+	return err
+}
