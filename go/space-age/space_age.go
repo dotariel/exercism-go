6f4d34628f225234410137b05622e1de540a2bef
Ariel Alonso <xsaint32@gmail.com>
2017-11-12 11:40:38 -0500

    Improve 'space-age' solution to be faster

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -6,19 +6,19 @@ type Planet string
 // Earth represents the baseline orbital age for Earth
 const Earth float64 = 31557600
 
+var conversions = map[Planet]float64{
+	"Earth":   Earth,
+	"Mercury": Earth * 0.2408467,
+	"Venus":   Earth * 0.61519726,
+	"Mars":    Earth * 1.8808158,
+	"Jupiter": Earth * 11.862615,
+	"Saturn":  Earth * 29.447498,
+	"Uranus":  Earth * 84.016846,
+	"Neptune": Earth * 164.79132,
+}
+
 // Age computes the age in a given Planet
 func Age(a float64, p Planet) float64 {
-	conversions := map[Planet]float64{
-		"Earth":   Earth,
-		"Mercury": Earth * 0.2408467,
-		"Venus":   Earth * 0.61519726,
-		"Mars":    Earth * 1.8808158,
-		"Jupiter": Earth * 11.862615,
-		"Saturn":  Earth * 29.447498,
-		"Uranus":  Earth * 84.016846,
-		"Neptune": Earth * 164.79132,
-	}
-
 	return round(a / conversions[p])
 }
 
