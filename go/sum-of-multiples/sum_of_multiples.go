819b9fc5544c9c3ac9cf1e12382ca00f63ffb1e3
Ariel Alonso <xsaint32@gmail.com>
2017-11-16 16:54:27 -0500

    Improve 'sum-of-multiples' solution

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -2,16 +2,20 @@ package summultiples
 
 func SumMultiples(limit int, divisors ...int) (sum int) {
 	for i := 1; i < limit; i++ {
-		multiples := make(map[int]bool)
-		for _, d := range divisors {
-			if i%d == 0 {
-				if _, added := multiples[i]; !added {
-					sum += i
-					multiples[i] = true
-				}
-			}
+		if isMultipleOfAny(i, divisors...) {
+			sum += i
 		}
 	}
 
-	return sum
+	return
+}
+
+func isMultipleOfAny(n int, divisors ...int) bool {
+	for _, d := range divisors {
+		if n%d == 0 {
+			return true
+		}
+	}
+
+	return false
 }
