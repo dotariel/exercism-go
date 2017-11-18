d59ef85f175ddb3e0f875b5707f472fcd4eb1b34
Ariel Alonso <xsaint32@gmail.com>
2017-11-18 15:03:58 -0500

    Add 'bank-account' solution

---
+++ go/bank-account/bank_account.go
@@ -0,0 +1,63 @@
+package account
+
+import (
+	"sync"
+)
+
+type Account struct {
+	sync.Mutex
+	balance int64
+	open    bool
+}
+
+func Open(initialDeposit int64) *Account {
+	if initialDeposit < 0 {
+		return nil
+	}
+
+	return &Account{balance: initialDeposit, open: true}
+}
+
+func (a *Account) Close() (payout int64, ok bool) {
+	a.Lock()
+	defer a.Unlock()
+
+	if !a.open {
+		return a.balance, false
+	}
+
+	payout = a.balance
+	a.balance = 0
+	a.open = false
+
+	return payout, true
+}
+
+func (a *Account) Balance() (balance int64, ok bool) {
+	a.Lock()
+	defer a.Unlock()
+
+	if !a.open {
+		return 0, false
+	}
+
+	return a.balance, true
+}
+
+func (a *Account) Deposit(amount int64) (int64, bool) {
+	a.Lock()
+	defer a.Unlock()
+
+	if !a.open {
+		return a.balance, false
+	}
+
+	newBalance := a.balance + amount
+	if newBalance < 0 {
+		return a.balance, false
+	}
+
+	a.balance = newBalance
+
+	return a.balance, true
+}
