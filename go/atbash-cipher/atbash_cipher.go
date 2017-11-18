32dfa1e845c15279a018b1fd7a0cbe70b6751ad8
Ariel Alonso <xsaint32@gmail.com>
2017-11-18 17:41:31 -0500

    Add 'atbash-cipher' solution

---
+++ go/atbash-cipher/atbash_cipher.go
@@ -0,0 +1,43 @@
+package atbash
+
+import (
+	"strings"
+	"unicode"
+)
+
+const (
+	key string = "abcdefghijklmnopqrstuvwxyz"
+)
+
+func Atbash(plain string) string {
+	return format(strings.Map(mapf, strip(plain)))
+}
+
+func mapf(r rune) rune {
+	if !unicode.IsLower(r) {
+		return r
+	}
+
+	cidx := (26 - strings.IndexRune(key, r)) - 1
+
+	return rune(key[cidx])
+}
+
+func strip(s string) (res string) {
+	for _, char := range strings.ToLower(s) {
+		if unicode.In(char, unicode.Lower, unicode.Digit) {
+			res += string(char)
+		}
+	}
+	return
+}
+
+func format(cipher string) (res string) {
+	for i, char := range cipher {
+		res += string(char)
+		if (i+1)%5 == 0 && i != len(cipher)-1 {
+			res += string(32)
+		}
+	}
+	return
+}
